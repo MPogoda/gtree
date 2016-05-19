312b5819bd222961cec39ccb7b82c08ebe69c3dd
Michael Pogoda <mi.pogoda@samsung.com>
2016-05-19 16:48:57 +0300

    Initial commit.

---
+++ gtree/person.cxx
@@ -0,0 +1,15 @@
+#include "person.h"
+
+namespace gtree
+{
+std::string Person::firstName() const
+{
+    return "Bob";
+}
+
+std::string Person::lastName() const
+{
+    return "Smith";
+}
+} // namespace gtree
+
---
+++ gtree/person.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+namespace gtree
+{
+/*!
+ * \class Person
+ * \brief
+ */
+class Person
+{
+public:
+    std::string firstName() const;
+    std::string lastName() const;
+private: // functions
+private: // fields
+}; // class Person
+} // namespace gtree
+
